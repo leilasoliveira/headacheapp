98c3956e9e3b335233d9f010a3f9edd5a36cfd09
Leila Farias <leilafarias@MacBook-Air-de-Leila.local>
2025-07-05 21:20:11 -0300

    Initial commit

---
+++ src/main/java/br/com/softwareit/headacheapp/HeadacheappApplication.java
@@ -0,0 +1,13 @@
+package br.com.softwareit.headacheapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadacheappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadacheappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softwareit/headacheapp/HeadacheappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softwareit.headacheapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadacheappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
